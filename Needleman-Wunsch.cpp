036fad49cab279e23cf285e1e5dad58208c8306d
Sohil <sohilchavan@gmail.com>
2024-05-07 16:34:04 -0700

    Refactored code for dynamic allocation and added helper functions

--- Needleman-Wunsch.cpp
+++ Needleman-Wunsch.cpp
@@ -11,97 +11,90 @@
 using namespace std;
 
 // Optimal Block Size (Number of threads per block, higher is better for older GPUs)
-const int BLOCK_SIZE = 1024;
-const int THREAD_MAX_PER_MS = 8000;
-
-// GPU Kernel function to fill distance matrix
-global__ void fillBlock(float *distm, int rows, int cols, int minSql, int maxSql, int diag, int numBlocks, float *result){
-    // Assign each thread to a cell
-    int tx = threadIdx.x;
-    int ty = threadIdx.y * 
-    int inBlockIndex = tx + ty * blockDim.x;
-
-    // Assign min and max for sequences
-
-    int min;
-    int max;
-    
-    min = minSeql;
-    max = maxSeql;
+const long int GRID_SIZE = 1024; // Number of blocks on the grid (X dimension)
+const int BLOCK_SIZE = 1024; // Number of threads on each block
 
 
-    if (i < min){
+// Get sequence from File
+string getSeq(string fName){
+    ifstream infile(fName);
+    if (!infile){
+        cerr << "Could not open file: " << fname << "\n";
+        return 1;
+    }
+    string seq;
+    infile >> seq;
+    infile.close();
+    return seq;
+}
 
-        // Convert block ID to block #
-        int blockNum = (gridDim.x * blockIdx.y) + blockIdx.x;
-
-        // Get Y value of current cell
-        int zeroCellY = threadIdx.x + (sqrt(blockSize) * (numBlocks - 1) - ((numBlock - (blockNum + 1)) * blockSize));
-
-        // Get X value of current cell
-        int zeroCellX = threadIdx.y + (sqrt(blockSize) * (numBlocks - 1) - (blockNum * blockSize));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        sqrt(blockSize * blockIDx.x)
-        
-    } else if ((i >= min) && (i <= max)){
-        
-    } else {
-        
+// Convert characters to numerical form
+string convSeq(string *seq){
+    string result;
+    for (char base : *seq) {
+        switch (base) {
+            case 'A':
+                result += '1';
+                break;
+            case 'T':
+                result += '2';
+                break;
+            case 'C':
+                result += '3';
+                break;
+            case 'G':
+                result += '4';
+                break;
+            case '-':
+                result += '0';
+                break;
+            case ' ':
+                result += '0';
+                break;
+            default:
+            // To handle unrecognized characters
+                break;
+        }
     }
 
+    return result;
+}
 
-    // Find the x and y from the block ID
-        // When nblocks = diag
-        // When nblocks > diag
-        // When nblocks 
+// Find next start xy for diagonal filling
+vector<int> getNextXY(vector<int> *prev, int *r, int *c, long long int *t){
+    // Get b of y=-x+b that runs through previous start xy
+    b = prev[1] + prev[0];
 
+    // Find where y=-x+b intercepts the bounds
+    int y1 = -c + b;
+    int x1 = b - y1;
+    int y2 = 0;
+    int x2 = b;
+    if ((x2 > x1) && (y2 < y1)){
+        minX = x1;
+        maxY = y1;
+    } else if ((y2 = y1) && (x2 = x1)){
+        minX = x1;
+        maxY = y1;
+    } else {
+        minX = x2;
+        maxY = y2;
+    }
 
-    (sqrt(blockSize) * (diag + 1)) - (sqrt(blockSize) * (blockIdx.y))
-
-    // If block ID is 0, put the corner cell at the corresponding cell on the diagonal
-
-    // If the cell is out of bounds of the matrix, do nothing and return;
-
-    // If the cell is is the first row only or first column only and NOT in 0,0 , add the gap value and return;
-
-    // If the cell is at 0,0 , add 0 and return
-
-    // Else
-        // Empty while loop if distm[previous diagonal] or distm[previous horizontal] or distm[previous vertical] is null
-            // __Call threadfence_system();
-        // Complete computation
+    // Get number of out of bound threads
+    vector<int> exp = {prev[0] + t, prev[1] + t};
 }
 
 
 
+// Assume dismt is a reference to a 2d array in linear form
+__global__ void fillCell(float *distm, int rows, int cols, int start, int *seq){
+    int cellNum = blockIdx.x * blockDim.x + threadIdx.x;
 
 
+    //
 
-
-
-
+}
 
 
 
@@ -115,34 +108,30 @@ int main(int argc, char*argv[]) {
 
     cout << "Opening Files" << "\n";
 
-    // Open files
-    ifstream infile1(argv[1]);
-    ifstream infile2(argv[2]);
+    // Open files and read contents to string
+    string* seq1a = new string;
+    string* seq2a = new string;
+    *seq1a = getSeq(argv[1]);
+    *seq2a = getSeq(argv[2]);
 
-    if (!infile1 or !infile2){
-        cerr << "Could not open files" << "\n";
-        return 1;
-    }
-
-    // Read file contents to string
-    string seq1;
-    string seq2;
-    infile1 >> seq1;
-    infile2 >> seq2;
-
-    // Close files
-    infile1.close();
-    infile2.close();
-
-    cout << "Reading strings: " << seq1 << ", " << seq2 << "\n";
+    // cout << "Reading strings: " << seq1 << ", " << seq2 << "\n";
 
     // Get lengths of strings
     int seq1l;
     int seq2l;
-    seq1l = seq1.length();
-    seq2l = seq2.length();
+    seq1l = seq1a.length();
+    seq2l = seq2a.length();
 
-    cout << "Getting lengths: " << seq1l << ", " << seq2l << "\n";
+    // Convert strings to numerical form
+    // A = 1, T = 2, C = 3, G = 4, gap = 0
+    string* seq1 = new string;
+    string* seq2 = new string;   
+    *seq1 = convSeq(seq1a);
+    *seq2 = convSeq(seq2a);
+    delete seq1a;
+    delete seq2a;
+
+    // cout << "Getting lengths: " << seq1l << ", " << seq2l << "\n";
 
     // Create distance matrix
     int** distm = new int*[seq1l + 1];
@@ -178,78 +167,6 @@ int main(int argc, char*argv[]) {
     cout << "rows: " << seq1l << "\n";
     cout << "columns: " << seq2l << "\n";
 
-    // Allocate memory on GPU for matrix (matrices on GPU are linear in structure)
-    // Allocate memory on GPU for result matrix
-    // Use float for GPU compatability/optimization
-    float *g_res;
-    float *g_mat;
-    cudaMalloc(&g_mat, (seq1l + 1) * (seq2l + 1) * sizeof(float));
-    cudaMalloc(&g_res, (seq1l + 1) * (seq2l + 1) * sizeof(float));
-
-    // Copy matrix to GPU
-    cudaMemcpy(g_mat, distm, seq1l * seq1l * sizeof(float), cudaMemcpyHostToDevice);
-
-    // Sequentially call diagonals of blocks
-        // Calculate the number of blocks for the current diagonal of blocks
-
-        // For each diagonal, call a kernel function to process the diagonal in blocks
-
-        // Synchronize globally between each diagonal block call
-
-        // For loop within for loop to handle max number of blocks processing part of a diagonal
-
-
-    int numDiags = ceil((seq1l + seq2l - 1) / sqrt(BLOCK_SIZE));
-    int minSeql;
-    int maxSeql;
-
-    if (seq1l >= seq2l){
-        minSeql = seq2l;
-    } else {
-        minSeql = seq1l
-    }
-
-    if (seq1l >= seq2l){
-        maxSeql = seq1l;
-    } else {
-        maxSeql = seq2l
-    }
-
-    for (i = 0; i < numDiags; i++){
-        // Calculate number of blocks for each diagonal
-        if (i < minSeql){
-            numBlocks = i + 1;
-        } else if ((i >= minSeql) && (i <= maxSeql)){
-            numBlocks == ceil(minSeql / sqrt(BLOCK_SIZE));
-        } else {
-            numBlocks == numDiags - i;
-        }
-
-        // Synchronize so that next threads wait
-        cudaDeviceSynchronize();
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -257,6 +174,8 @@ int main(int argc, char*argv[]) {
         delete[] distm[i];
     }
     delete[] distm;
+    delete seq1;
+    delete seq2;
 
     return 0;
 
--- Scratch.cpp
+++ Scratch.cpp
@@ -172,3 +172,197 @@ long long maxThreadCalc(int s1l, int s2l, int maxThreads){
 
     return staticDiagThreads;
 }
+
+
+if (phase == 0){
+
+    // Get Y value of current cell
+    int cellR = threadIdx.x + (blockWidth * (numBlocks - 1) - ((numBlock - (blockNum + 1)) * blockWidth));
+
+    // Get X value of current cell
+    int cellC = threadIdx.y + (blockWidth * (numBlocks - 1) - (blockNum * blockWidth));
+    
+} else if (phase == 1){
+
+    if (rows > cols){
+        // Means rows is minsql
+        // Get Y value of current cell
+        int cellR = threadIdx.x + (blockWidth * (numBlocks - 1) - ((numBlock - (blockNum + 1)) * blockWidth)) + blockWidth * (diag - floor(cols / blockWidth));
+
+        // Get X value of current cell
+        int cellC = threadIdx.y + (blockWidth * (numBlocks - 1) - (blockNum * blockWidth));
+
+    } else if (rows < cols){
+        // Means cols is minsql
+        // Get Y value of current cell
+        int cellR = threadIdx.x + (blockWidth * (numBlocks - 1) - ((numBlock - (blockNum + 1)) * blockWidth));
+
+        // Get X value of current cell
+        int cellC = threadIdx.y + (blockWidth * (numBlocks - 1) - (blockNum * blockWidth)) + blockWidth * (diag - floor(rows / blockwidth));
+
+    } else if (phase == 2){
+
+        // Get Y value of current cell
+        int cellR = threadIdx.x + (blockWidth * (numBlocks - 1) - ((numBlock - (blockNum + 1)) * blockWidth));
+
+        // Get X value of current cell
+        int cellC = threadIdx.y + (blockWidth * (numBlocks - 1) - (blockNum * blockWidth));
+
+    }
+
+} else {
+    
+    // Get Y value of current cell
+    int cellY = ;
+
+    // Get X value of current cell
+    int cellX = ;
+    
+}
+
+
+// Functions diagToCartesianX and Y
+// Takes in a pointer to array of size x*y, 
+// And the number of the diagonal perpendicular to 0,0
+// And the number of the cell on the diagonal
+// Then returns that cell's cartesian coordinates
+__device__ int diagToCartX(x, y, d, c){
+
+    
+
+}}
+
+__device__ int diagToCartY(){
+
+}
+
+// GPU Kernel function to fill distance matrix
+global__ void fillBlock(float *distm, int rows, int cols, int phase, int minSql, int maxSql, int diag, int numBlocks, float *result){
+    // Assign each thread to a cell
+    int tx = threadIdx.x;
+    int ty = threadIdx.y * 
+    int inBlockIndex = tx + ty * blockDim.x;
+
+    // Assign min and max for sequences
+
+    int min;
+    int max;
+    
+    min = minSql;
+    max = maxSql;
+
+    // Convert block ID to block #
+    int blockNum = (gridDim.x * blockIdx.y) + blockIdx.x;
+    int blockWidth = sqrt(blockSize);
+
+    
+
+
+    // If block ID is 0, put the corner cell at the corresponding cell on the diagonal
+
+    // If the cell is out of bounds of the matrix, do nothing and return;
+
+    // If the cell is is the first row only or first column only and NOT in 0,0 , add the gap value and return;
+
+    // If the cell is at 0,0 , add 0 and return
+
+    // Else
+        // Empty while loop if distm[previous diagonal] or distm[previous horizontal] or distm[previous vertical] is null
+            // __Call threadfence_system();
+        // Complete computation
+}
+
+// GPU Kernel function to fill distance matrix
+global__ void fillBlock(float *distm, float *result){
+    // Assign each thread to a cell
+    int tx = threadIdx.x;
+    int ty = threadIdx.y * 
+    int inBlockIndex = tx + ty * blockDim.x;
+
+    // Assign min and max for sequences
+
+    int min;
+    int max;
+    
+    min = minSql;
+    max = maxSql;
+
+    // Convert block ID to block #
+    int blockNum = (gridDim.x * blockIdx.y) + blockIdx.x;
+    int blockWidth = sqrt(blockSize);
+
+    
+
+
+    // If block ID is 0, put the corner cell at the corresponding cell on the diagonal
+
+    // If the cell is out of bounds of the matrix, do nothing and return;
+
+    // If the cell is is the first row only or first column only and NOT in 0,0 , add the gap value and return;
+
+    // If the cell is at 0,0 , add 0 and return
+
+    // Else
+        // Empty while loop if distm[previous diagonal] or distm[previous horizontal] or distm[previous vertical] is null
+            // __Call threadfence_system();
+        // Complete computation
+}
+
+    // Allocate memory on GPU for matrix (matrices on GPU are linear in structure)
+    // Allocate memory on GPU for result matrix
+    // Use float for GPU compatability/optimization
+    float *g_res;
+    float *g_mat;
+    cudaMalloc(&g_mat, (seq1l + 1) * (seq2l + 1) * sizeof(float));
+    cudaMalloc(&g_res, (seq1l + 1) * (seq2l + 1) * sizeof(float));
+
+    // Copy matrix to GPU
+    cudaMemcpy(g_mat, distm, seq1l * seq1l * sizeof(float), cudaMemcpyHostToDevice);
+
+    // Sequentially call diagonals of blocks
+        // Calculate the number of blocks for the current diagonal of blocks
+
+        // For each diagonal, call a kernel function to process the diagonal in blocks
+
+        // Synchronize globally between each diagonal block call
+
+        // For loop within for loop to handle max number of blocks processing part of a diagonal
+
+
+    int numDiags = ceil((seq1l + seq2l - 1) / sqrt(BLOCK_SIZE));
+    int minSeql;
+    int maxSeql;
+    int numBlocks;
+    int phase;
+
+    if (seq1l >= seq2l){
+        minSeql = seq2l;
+    } else {
+        minSeql = seq1l
+    }
+
+    if (seq1l >= seq2l){
+        maxSeql = seq1l;
+    } else {
+        maxSeql = seq2l
+    }
+
+    for (i = 0; i < numDiags; i++){
+        // Calculate number of blocks for each diagonal
+        if (i < minSeql){
+            numBlocks = i + 1;
+            phase = 0;
+        } else if ((i >= minSeql) && (i <= maxSeql)){
+            numBlocks == ceil(minSeql / sqrt(BLOCK_SIZE));
+            phase = 1;
+        } else {
+            numBlocks == numDiags - i;
+            phase = 2;
+        }
+
+        // Synchronize so that next threads wait
+        cudaDeviceSynchronize();
+
+        // Rows and Cols arg for kernels should be seq1l + 1 or seq2l + 2 for example
+
+    }
